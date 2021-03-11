e6e5cb9b670b670e30b8199314b89dc7763ac97d
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-03-11 18:21:18 +0900

    validatesの書き換え、before_action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :user_has_room, only: [:new, :create]
 
   def index
     
@@ -24,4 +26,12 @@ class RoomsController < ApplicationController
   def room_params
     params.require(:rooms_tag).permit(:image, :title, :content, :place_id, :floor_id, :style_id, :name).merge(user_id: current_user.id)
   end
+
+  def user_has_room
+    if Room.exists?(current_user.id)
+      flash[:notice] = 'すでにお部屋があります'
+      redirect_to root_path
+    end
+  end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -5,11 +5,6 @@ class Room < ApplicationRecord
 
   has_one_attached :image
 
-  with_options presence: true do
-    validates :title
-    validates :content
-    validates :image
-  end
 
 
   # ↓activehash関連
@@ -17,14 +12,6 @@ class Room < ApplicationRecord
   belongs_to :place
   belongs_to :floor
   belongs_to :style
-
-
-  
-  with_options numericality: { other_than: 1 }, presence: true do
-    validates :place_id
-    validates :floor_id
-    validates :style_id
-  end
 
 
 end
