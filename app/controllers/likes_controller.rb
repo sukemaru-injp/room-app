d4017f2947bb673c9395a353beaa5c790f694cc3
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-03-17 14:38:01 +0900

    dbへ保存んされる

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,19 @@
 class LikesController < ApplicationController
-  def create
+  before_action :set_room
 
+  def create
+    if @room.user_id != current_user.id
+    @likes = Like.create(user_id: current_user.id, room_id: @room.id)
+    redirect_to root_path
+    end
   end
 
   def destroy
-    
+
+  end
+
+  private 
+  def set_room
+    @room = Room.find(params[:room_id])
   end
 end
