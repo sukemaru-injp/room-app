5f3e54d2be7df7afdb502ff8c7bf023f079bc101
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-03-18 10:13:55 +0900

    テストの実行

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,6 +13,33 @@ RSpec.describe Comment, type: :model do
         expect(@comment).to be_valid
       end
     end
+
+    context "投稿できない" do
+      it "textが空では投稿できない" do
+        @comment.text = ""
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it "textが140字以上では投稿できない" do
+        @comment.text = "a"*141
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text is too long (maximum is 140 characters)")
+      end
+
+      it "user_idが空では登録できない" do
+        @comment.user_id = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+      end
+
+      it "room_idが空では登録できない" do
+        @comment.room_id = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Room must exist")
+      end
+
+    end
   end
 
 end
