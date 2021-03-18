1c95b067c74be0dfa5b72a3ee928ea80155a2057
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-03-18 15:50:31 +0900

    likeモデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name              { 'techcamp'}
-    email                 { 'test@test'}
+    email                 {Faker::Internet.free_email}
     password              { 'test123' }
     password_confirmation { password }
     sex_id                { 2 }
